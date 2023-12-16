3bffeaecd6b15b968a26251e60363451934df30c
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2023-12-16 11:00:34 +0630

    add global pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CardsModule } from './cards/cards.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ChargesModule } from './charges/charges.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { CardsModule } from './cards/cards.module';
-import { ChargesModule } from './charges/charges.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, CardsModule, ChargesModule],
+  imports: [
+    CardsModule,
+    MongooseModule.forRoot('mongodb://localhost/payment'),
+    ChargesModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
